74bbb28526e99f5e81ae68fbcf5bd3a7bc048be8
Moks-Hold <samuel.brumfield2@gmail.com>
2017-12-12 10:08:34 -0500

    Teams Done

--- team14.py
+++ team14.py
@@ -1,8 +1,8 @@
-team_name = 'EZ'
+team_name = 'Mok'
 strategy_name = 'Winning'
 strategy_description = 'Winning'
 import random
     
 def move(my_history, their_history, my_score, their_score):
           
-    return random.choice([z,t,y,p])
+    return random.choice([1,2,3,4])
