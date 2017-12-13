a0356319c343a117a805f20b5cc9a02b6c37c18d
BigDallyD <18dburchfield@MCHS-S-PLTW-008.madison.lcl>
2017-12-13 10:01:15 -0500

    Dallas
    Teams

--- team5.py
+++ team5.py
@@ -9,10 +9,9 @@
 team_name = 'The name the team gives to itself' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
-    
+
 def move(my_history, their_history, my_score, their_score):
-    ''' Arguments accepted: my_history, their_history are strings.
-    my_score, their_score are ints.
+    '''Make my move based on the history with this player.
     
     Make my move.
     Returns 'c' or 'b'. 
@@ -53,7 +52,7 @@ if __name__ == '__main__':
               my_score=0,
               their_score=0,
               result='b'):
-         print('Test passed')
+         print ('Test passed')
      # Test 2: Continue betraying if they collude despite being betrayed.
     test_move(my_history='bbb',
               their_history='ccc', 
